0ef90d542a7043ac14fd7ef03c01f149630a5ca2
Karys Chang <karyschang@Karyss-MacBook-Air.local>
2023-09-17 01:36:34 -0700

    intro to software workshop helloWorld

---
+++ src/main/java/frc/robot/HelloWorld.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class HelloWorld {
+    public static void helloWorld(){
+        System.out.println("hello world!");
+    }
+
+    // public static void main(String[] args){
+    //     System.out.println("testing...");
+    // }
+}
