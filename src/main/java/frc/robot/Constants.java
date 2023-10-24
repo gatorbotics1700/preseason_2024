15decf9fc28462d1e9d2e3958f92e428f9a4548a
averyneuner <24aneuner@castilleja.org>
2023-10-24 16:47:35 -0700

    filled in constants and used them in drive code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,4 +2,12 @@ package frc.robot;
 
 public class Constants {
     //initialize TICKS_PER_REV, METERS_PER_INCH
+    public static final double TICKS_PER_REV = 2048.0; 
+    public static final double METERS_PER_INCH = 0.0254;
+
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.4960872;
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.4960872;
+
+    public static final int DRIVETRAIN_PIGEON_ID = 6; 
+    
 }
