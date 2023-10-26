0cb14d51dd3fc61a871dccdb87fa41f6e9ea2636
Poppy Scott <poppy@Poppy-Scott.local>
2023-10-25 19:39:31 -0700

    updated drive code

--- src/main/java/frc/com/swervedrivespecialties/swervelib/SwerveModule.java
+++ src/main/java/frc/com/swervedrivespecialties/swervelib/SwerveModule.java
@@ -1,9 +1,12 @@
 package frc.com.swervedrivespecialties.swervelib;
 
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+
 public interface SwerveModule {
     double getDriveVelocity();
 
     double getSteerAngle();
 
+    SwerveModulePosition getSwerveModulePosition();
     void set(double driveVoltage, double steerAngle);
 }
